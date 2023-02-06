92db13c46ce69af8494b71644f2f00c8c246d5eb
guilherme <guilherme.md@gmail.com>
2023-02-06 15:53:28 -0300

    CRUD working

---
+++ index.js
@@ -0,0 +1,36 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const app = express();
+
+require("dotenv").config();
+
+app.use(
+  express.urlencoded({
+    extended: true,
+  })
+);
+
+app.use(express.json());
+
+// rotas da API
+const personRoutes = require("./routes/personRoutes");
+
+app.use("/person", personRoutes);
+
+app.get("/", (req, res) => {
+  res.json({ message: "Oi Express!" });
+});
+
+const DB_USER = process.env.DB_USER;
+const DB_PASSWORD = encodeURIComponent(process.env.DB_PASSWORD);
+
+mongoose.set("strictQuery", false);
+mongoose
+  .connect(
+    `mongodb+srv://${DB_USER}:${DB_PASSWORD}@cluster0-classicriders.qmdgrez.mongodb.net/bancodaapi?retryWrites=true&w=majority`
+  )
+  .then(() => {
+    console.log("Contectamos on MongoDB!");
+    app.listen(3001);
+  })
+  .catch((err) => console.log(err));
